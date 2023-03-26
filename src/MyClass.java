b10eddae678f760aa5c23fa1f7fa821bd6c7dc85
Evelina Ivanna <evelinka830@gmail.com>
2023-03-26 19:38:12 +0300

    Home work 1

---
+++ src/MyClass.java
@@ -0,0 +1,11 @@
+public class MyClass {
+    private static int sumOfTwoNumbers(int num1, int num2) {
+
+        return num1 + num2;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(sumOfTwoNumbers(5, 2));
+    }
+}
